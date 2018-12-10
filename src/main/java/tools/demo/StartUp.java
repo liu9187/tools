48d3318fdce915787ba89364af81427ddd160d2d
liucl <liucl@dehuinet.com>
2018-12-10 11:17:04 +0800

    增加md5签名

--- src/main/java/tools/demo/StartUp.java
+++ src/main/java/tools/demo/StartUp.java
@@ -2,11 +2,6 @@ package tools.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import tools.demo.test.WebTest;
-
-import javax.xml.ws.Endpoint;
 @SpringBootApplication
 public class StartUp {
 
